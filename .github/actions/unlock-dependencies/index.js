65e6777f09a6a0da95dfca5d34b013c25968b91d
Andrei Zhaleznichenka <zhalea@amazon.de>
2023-07-24 15:22:33 +0200

    chore: Unlock dependencies for main package and bundle size action

--- .github/actions/unlock-dependencies/index.js
+++ .github/actions/unlock-dependencies/index.js
@@ -8,22 +8,44 @@ const path = require("path");
 /**
  * Remove specific @cloudscape-design/* packages where we should always use the latest minor release.
  */
-const filename = path.resolve(process.env.GITHUB_WORKSPACE, "package-lock.json");
-const packageLock = JSON.parse(fs.readFileSync(filename));
+[
+  path.resolve(process.env.GITHUB_WORKSPACE, "package-lock.json"),
+  path.resolve(
+    process.env.GITHUB_WORKSPACE,
+    ".github",
+    "workflows",
+    "bundle-size",
+    "package-lock.json"
+  ),
+].forEach((filename) => unlock(filename));
+
+function unlock(filename) {
+  try {
+    const packageLock = JSON.parse(fs.readFileSync(filename));
+
+    Object.keys(packageLock.packages).forEach((dependencyName) => {
+      removeDependencies(dependencyName, packageLock.packages);
+    });
+
+    Object.keys(packageLock.dependencies).forEach((dependencyName) => {
+      removeDependencies(dependencyName, packageLock.dependencies);
+    });
+
+    fs.writeFileSync(filename, JSON.stringify(packageLock, null, 2) + "\n");
+
+    console.log(
+      "Removed @cloudscape-design/ dependencies from package-lock file"
+    );
+  } catch (error) {
+    console.warn(
+      `Can't unlock dependencies for ${filename}: `,
+      error?.message ?? "unknown reason"
+    );
+  }
+}
 
 function removeDependencies(dependencyName, packages) {
   if (dependencyName.includes("@cloudscape-design/")) {
     delete packages[dependencyName];
   }
 }
-
-Object.keys(packageLock.packages).forEach((dependencyName) => {
-  removeDependencies(dependencyName, packageLock.packages);
-});
-
-Object.keys(packageLock.dependencies).forEach((dependencyName) => {
-  removeDependencies(dependencyName, packageLock.dependencies);
-});
-
-fs.writeFileSync(filename, JSON.stringify(packageLock, null, 2) + "\n");
-console.log("Removed @cloudscape-design/ dependencies from package-lock file");
